46fa6f275557910b510eb5ecfb2e954619c43acf
jackmattog <jackmattog@gmail.com>
2026-05-19 05:17:48 +0300

    add a simple home template,its view and url to test tailwindcss and it worked

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from .views import HomeView
 
 #App name 
 app_name = "core"
 
 urlpatterns = [
-    
+    path("",HomeView.as_view(),name="home")
 ]
