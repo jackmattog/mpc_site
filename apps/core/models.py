46fc2b12b8f1653c39587c571cb8b8c23b0013a7
jackmattog <jackmattog@gmail.com>
2026-05-31 08:38:58 +0300

    feature:add suggestion model which save users informations when gives mpc a suggestion

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Suggestion(models.Model):
+    name = models.CharField(max_length=100)
+    contact_details = models.CharField(max_length=100, help_text="Phone number or WhatsApp")
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"Message from {self.name} - {self.created_at.strftime('%Y-%m-%d')}"
