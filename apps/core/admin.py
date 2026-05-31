a940703409b7cf7817b645257e280e2e579fead5
jackmattog <jackmattog@gmail.com>
2026-05-31 10:00:11 +0300

    add suggestion model to admin panel and make it imposible to edit as all editions are done by user

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Suggestion
 
-# Register your models here.
+@admin.register(Suggestion)
+class SuggestionAdmin(admin.ModelAdmin):
+    # These are the columns that will show up in the admin list view
+    list_display = ('name', 'contact_details', 'created_at')
+    
+    # A search bar to easily find specific messages
+    search_fields = ('name', 'contact_details', 'message')
+    
+    # A filter sidebar to filter by date
+    list_filter = ('created_at',)
+    
+    # Makes the records read-only to avoid me to edit
+    readonly_fields = ('name', 'contact_details', 'message', 'created_at')
+
+    # Prevents adding new suggestions from the admin panel (since users do this)
+    def has_add_permission(self, request):
+        return False
