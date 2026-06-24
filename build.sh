1b57dd67a2628b0c0283e14c7249a099b7371a02
jackmattog <jackmattog@gmail.com>
2026-06-24 09:46:52 +0300

    remove migrate in build.sh file for it not to run migrate in the build stage

--- build.sh
+++ build.sh
@@ -11,9 +11,5 @@ npm run build
 # Install dependencies
 pip install -r requirements.txt
 
-
-# Run database migrations
-python manage.py migrate
-
 # Collect static files for Tailwind/CSS
 python manage.py collectstatic --no-input
